d7dcf815e969300611c1763ef2095e5f65cb7330
k-koech <kelvin.koech@student.moringaschool.com>
2022-06-08 19:56:21 +0300

    commit tree d692ae905917ac26f7ed26d9a76d17d4e010d8db

--- looping.rb
+++ looping.rb
@@ -1,5 +1,15 @@
 def happy_new_year
-  # your code here
+  i = 11
+  loop do
+    i = i - 1
+    puts i
+    
+    if i==1
+      break
+    end# this will cause execution to exit the loop
+  end
+
+  puts "Happy New Year!"
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
@@ -16,9 +26,22 @@ def fizzbuzz(num)
 end
 
 def fizzbuzz_printer
-  # your code here
+  101.times do |num|
+    if num==0 
+      next
+    elsif num % 3 == 0 && num % 5 == 0
+      puts "FizzBuzz"
+    elsif num % 3 == 0 && num % 5 != 0
+      puts "Fizz"
+    elsif num % 5 == 0 && num % 3 != 0
+      puts "Buzz"
+    
+    else
+      puts num
+    end
+  end
 end
 
 def reverse_string(str)
-  # your code here
+  str.chars.reduce { |x, y| y + x } 
 end
